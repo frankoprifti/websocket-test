e45a938d92a019e5ecf2aa0302990fe667a28c29
FP <frankoprifti@gmail.com>
2021-05-12 12:05:57 +0200

    Icon

--- src/file.js
+++ src/file.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function File({ msg, download, filename, user }) {
     return (
         <div onClick={() => download(msg.text, msg.user + "-" + filename)} className={`file-wrap ${msg.user === user ? 'my-file' : 'nope'}`}>
-            <img alt={'fileicon'} className='icon' src='https://www.flaticon.com/svg/vstatic/svg/337/337956.svg?token=exp=1620387772~hmac=9dce1c2fdc079aa98803422cdbd93ca6' />
+            <img alt={'fileicon'} className='icon' src='https://archive.org/download/txt-file-20-504249/txt-file-20-504249.png' />
             <p className='filename '>{msg.user}</p>
         </div>
     )
